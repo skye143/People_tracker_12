39fe171c8feab72a2ecdf9a3bf3296712af81df3
Skye143 <skyeheredia@gmail.com>
2023-05-14 23:36:27 -0400

    updating sqls

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,31 @@
 CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT NOT NULL,
-    CONSTRAINT FOREIGN KEY (department_id)
-    REFERENCES department (id) ON DELETE CASCADE
+    department_id INTEGER
+    -- department_id INT NOT NULL,
+    -- CONSTRAINT FOREIGN KEY (department_id)
+    -- REFERENCES department (id) ON DELETE CASCADE
 );
 
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT NOT NULL,
-     CONSTRAINT FOREIGN KEY (role_id)
-    REFERENCES role(id) ON DELETE CASCADE,
-    manager_id INT,
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
+    role_id INTEGER,
+    manager_id INTEGER
+    -- role_id INT NOT NULL,
+    --  CONSTRAINT FOREIGN KEY (role_id)
+    -- REFERENCES role(id) ON DELETE CASCADE,
+    -- manager_id INT,
+    -- FOREIGN KEY (manager_id)
+    -- REFERENCES employee(id)
 );
 
 
