6fd102c9f395fe803a95fb607e27f8c1dfe875c8
Skye143 <skyeheredia@gmail.com>
2023-05-17 15:41:12 -0400

    added the first prompt part

--- index.js
+++ index.js
@@ -15,3 +15,51 @@ const msg = () => {
         promptUser();
     });
 };
+
+
+
+const promptUser = () => {
+    inquirer
+        .prompt({
+            type: 'list',
+            name: 'startApplication',
+            message: 'What would you like to do?',
+            choices: ['View All Employees', 'View All Employees By Department', 'View All Employees By Manager', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
+        })
+        .then((data) => {
+            switch (data['startApplication']) {
+                case 'View All Employees':
+                    viewAllEmp();
+                    break;
+                case 'View All Employees By Department':
+                    viewEmpByDep();
+                    break;
+                case 'View All Employees By Manager':
+                    viewEmpByManager();
+                    break;
+                case 'Add Employee':
+                    addEmp();
+                    break;
+                case 'Update Employee Role':
+                    updateEmp();
+                    break;
+                case 'View All Roles':
+                    viewRoles();
+                    break;
+                case 'Add Role':
+                    addRole();
+                    break; 
+                case 'View All Departments':
+                    viewDep();
+                    break;
+                case 'Add Department':
+                    addDep();
+                    break;
+                case 'Quit':
+                    break;
+            }
+        })
+        .catch((error) => {
+            console.log('error');
+        })
+};
