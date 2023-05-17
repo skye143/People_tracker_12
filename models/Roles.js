8dd5cefedb3069bc4eb97b25358df9ac46d9999e
Skye143 <skyeheredia@gmail.com>
2023-05-17 15:44:58 -0400

    added exports and toles

--- models/Roles.js
+++ models/Roles.js
@@ -1,3 +1,32 @@
 const { promptUser} = require('../index');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    database: 'employee_manager_db',
+    user: 'root',
+    password: 'rootroot'
+});
+
+// View Roles //
+const viewRoles = () => {
+    connection.query(
+        `SELECT roles.id, roles.title, roles.salary, department.name
+            FROM roles
+            LEFT JOIN department
+            ON roles.department_id = department.id`,
+        function (error, results, fields) {
+            if (error) {
+                console.log(error.message);
+                return;
+            }
+            console.table(results);
+            promptUser();
+        }
+    );
+};
+
+
+
+module.exports = { viewRoles, addRole };
