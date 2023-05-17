6610fe21d7e2ffc89c492d6dfa9c86d9e972beeb
Skye143 <skyeheredia@gmail.com>
2023-05-17 15:45:36 -0400

    added the funtion to add a role

--- models/Roles.js
+++ models/Roles.js
@@ -27,6 +27,60 @@ const viewRoles = () => {
     );
 };
 
-
+// Add Role //
+const addRole = () => {
+    connection.query(
+        `SELECT * FROM department`,
+        function (error, results, fields) {
+            if (error) {
+                console.log(error);
+                return;
+            }
+            let depArray = [];
+            results.forEach(item => {
+                depArray.push(item.name)
+            })
+            inquirer
+                .prompt([
+                    {
+                        type: 'text',
+                        name: 'role_title',
+                        message: 'What is the name of the role you would like to add?'
+                    },
+                    {
+                        type: 'number',
+                        name: 'salary',
+                        message: 'Enter the salary of the new role. (DO NOT use commas or periods)  ?'
+                    },
+                    {
+                        type: 'list',
+                        name: 'department',
+                        message: 'What department does the new role belong to?',
+                        choices: depArray
+                    }
+                ])
+                .then ((data) => {
+                    let department_id;
+                    for (let i = 0; i < depArray.length; i++) {
+                        if (depArray[i] === data.department) {
+                            department_id = i + 1;
+                        };
+                    };
+                    connection.query(
+                        `INSERT INTO roles (title, salary, department_id) VALUES(?,?,?)`,
+                        [data.role_title, data.salary, department_id],
+                        function (error, results, fields) {
+                            if (error) {
+                                console.log(error.message);
+                                return;
+                            }
+                            console.log('New role added');
+                            promptUser();
+                        }
+                    );
+                });
+        }
+    );
+};
 
 module.exports = { viewRoles, addRole };
