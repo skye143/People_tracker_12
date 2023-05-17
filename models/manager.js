0293950552143ac8832c7a40a16ab52fd722febf
Skye143 <skyeheredia@gmail.com>
2023-05-17 15:48:23 -0400

    added the basic juntions needed and exported the file

--- models/manager.js
+++ models/manager.js
@@ -7,3 +7,73 @@ const connection = mysql.createConnection({
     user: 'root',
     password: 'rootroot'
 });
+
+
+// Deletes Manager Table //
+const dropManager = () => {
+    connection.query(
+        `DROP TABLE IF EXISTS manager`,
+        function (error, results, fields) {
+            if (error) {
+                console.log(error.message);
+            }
+        }
+    )
+};
+
+
+// Creates Manager Table //
+const createManagerTable = () => {
+    connection.query(
+        `CREATE TABLE manager (
+            id INT NOT NULL AUTO_INCREMENT,
+            first_name VARCHAR(30),
+            last_name VARCHAR(30),
+            PRIMARY KEY (id)
+        )`,
+        function (error, results, fields) {
+            if (error) {
+                console.log(error.message);
+            }
+        }
+    )
+};
+
+
+
+
+
+// Add Manager //
+const addManagers = () => {
+    connection.query(
+        `INSERT INTO manager (first_name, last_name)
+        SELECT first_name,
+            last_name
+        FROM employee
+        WHERE manager_confirm = 1`,
+        function (error, results, fields) {
+            if (error) {
+                console.log(error.message);
+            }
+        }
+    )
+};
+
+
+
+module.exports = { dropManager, createManagerTable, addManagers };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
