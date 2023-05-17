28c68ffa2913fddcf9b86adf284175527c535017
Skye143 <skyeheredia@gmail.com>
2023-05-17 15:50:34 -0400

    finished very similar to the last js folder will possibly updayte

--- models/Department.js
+++ models/Department.js
@@ -3,3 +3,58 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
 
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    database: 'employee_manager_db',
+    user: 'root',
+    password: 'rootroot'
+});
+
+
+
+
+
+// View Departments //
+const viewDep = () => {
+    connection.query(
+        `SELECT * FROM department`,
+        function (error, results, fields) {
+            if (error) {
+                console.log(error.message);
+                return;
+            }
+            console.table(results);
+            promptUser();
+        }
+    )
+};
+
+
+// Add Department //
+const addDep = () => {
+    inquirer
+        .prompt({
+            type: 'text',
+            name: 'dep_name',
+            message: 'What is the name of the depertment you would like to add?'
+        })
+        .then((data) => {
+            connection.query(
+                `INSERT INTO department (name) VALUES(?)`,
+                [data.dep_name],
+                function (error, results, fields) {
+                    if (error) {
+                        console.log(error.message);
+                        return;
+                    }
+                    console.log('New department has been added');
+                    promptUser();
+                }
+            )
+        })
+};
+
+
+
+module.exports = { viewDep, addDep }
